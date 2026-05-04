6f08d3de6e988575d3cb6d0cad932104e395153f
Flavien Charlon <flavien@charlon.org>
2026-05-05 00:34:38 +0100

    Handle nullable collections

--- alter-tests/src/test/kotlin/org/dynadoc/TestClasses.kt
+++ alter-tests/src/test/kotlin/org/dynadoc/TestClasses.kt
@@ -1,8 +1,26 @@
 package org.dynadoc
 
 import java.net.URI
-import org.dynadoc.Alter
+import java.time.DayOfWeek
 
-@Alter data class Scalars(val string: String, val integer: Int, val nullable: String?)
-@Alter data class Collections(val list: List<String>, val map: Map<String, Int>, val set: Set<Scalars>, val nullable: List<Long>?)
-@Alter data class NestedObjects(val alterable: Scalars, val nonAlterable: URI)
+@Alter
+data class Scalars(
+    val string: String,
+    val integer: Int,
+    val nullable: String?
+)
+
+@Alter
+data class Collections(
+    val list: List<String>,
+    val map: Map<DayOfWeek, Int>,
+    val set: Set<Scalars>,
+    val nullable: List<Long>?,
+)
+
+@Alter
+data class NestedObjects(
+    val alterable: Scalars,
+    val nonAlterable: URI,
+    val nullable: Scalars?,
+)
