8303a1ec13c5acf22437715edb7d86fc9f78dabf
Flavien Charlon <flavien@charlon.org>
2026-05-04 17:09:29 +0100

    Create the initial version of the project

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+﻿plugins {
+    kotlin("jvm") version "2.3.21" apply false
+    id("com.google.devtools.ksp") version "2.3.7" apply false
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+    }
+}
