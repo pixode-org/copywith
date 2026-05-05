dba47a5de7aed559a3783d61eb353a734b4f8f0a
Flavien Charlon <flavien@charlon.org>
2026-05-05 20:23:49 +0100

    Move the unit tests to the `alter-annotation` module

--- alter-annotation/build.gradle.kts
+++ alter-annotation/build.gradle.kts
@@ -1,5 +1,6 @@
 ﻿plugins {
     kotlin("jvm")
+    id("com.google.devtools.ksp")
 }
 
 group = "org.dynadoc"
@@ -7,4 +8,16 @@ version = "1.0.0"
 
 kotlin {
     jvmToolchain(25)
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("io.kotest:kotest-assertions-core:6.1.11")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:6.1.11")
+
+    kspTest(project(":alter-processor"))
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
